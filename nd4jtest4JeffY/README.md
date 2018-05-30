aba07e46407e3b0984bd818dd535b7749b1eac7a
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2018-05-30 08:02:36 -0700

    Adding ability to run from maven

---
+++ nd4jtest4JeffY/README.md
@@ -0,0 +1,9 @@
+### To run
+
+`mvn package`
+`mvn exec:java`
+
+### To build an executible jar
+
+`mvn package`
+The jar is in target with "dependencies" added.  For barebones about 26 megs.  Not yet tested on PC.
--- nd4jtest4JeffY/src/main/java/TestMain.java
+++ nd4jtest4JeffY/src/main/java/TestMain.java
@@ -1,3 +1,5 @@
+package org.zoe.nd4jtest4JeffY;
+
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -6,21 +8,18 @@ public class TestMain {
 
     public static void main(String[] args) {
 
+        int n = 20; // Size of array
+        String type = "row"; // Also col for column-major.
+        int iterations = 100;
 
-        System.out.println("Size:" + args[0]);
-        int n = 0;
-        try{
-          n =  Integer.parseUnsignedInt(args[0]);
-        } catch(NumberFormatException nfe) {
-            System.err.println("Size argument was not a parsable unsigned integer, exiting...");
-            System.exit(0);
-        }
         System.out.println("Creating random " + n + "x" + n + " Matrix using...");
+
         INDArray mat = null;
-        if(args[1].equalsIgnoreCase("c") || args[1].equalsIgnoreCase("row")) {
+
+        if(type.equalsIgnoreCase("c") || type.equalsIgnoreCase("row")) {
             mat = Nd4j.rand('c', new int[]{n,n});
             System.out.println("C aka row-major ordering...");
-        } else if (args[1].equalsIgnoreCase(("f")) || args[1].equalsIgnoreCase("col")) {
+        } else if (type.equalsIgnoreCase(("f")) || type.equalsIgnoreCase("col")) {
             mat = Nd4j.rand('f', new int[]{n,n});
             System.out.println("Fortran aka column-major ordering...");
         } else {
@@ -28,20 +27,13 @@ public class TestMain {
             System.exit(0);
         }
 
-        int iterations = 0;
-        try{
-            iterations =  Integer.parseUnsignedInt(args[2]);
-        } catch(NumberFormatException nfe) {
-            System.err.println("Number of iterations is nonsensical...");
-            System.exit(0);
-        }
 
         long st = System.nanoTime();
         for(int ii=0; ii<iterations; ii++) {
             mat = mat.muli(mat);
-            if ((ii + 1) % (int) (iterations * 0.01) == 0) {
-                System.out.print((ii + 1) / (int) (iterations * 0.01) + "%  ");
-            }
+            // if ((ii + 1) % (int) (iterations * 0.01) == 0) {
+            //     System.out.print((ii + 1) / (int) (iterations * 0.01) + "%  ");
+            // }
         }
         long ed = System.nanoTime();
         System.out.println();
